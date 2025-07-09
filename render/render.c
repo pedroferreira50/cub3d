aba3cc9376a64e823bb896c0c53b0a7da4bfd2ed
Grace <20680751+godonnell2@users.noreply.github.com>
2025-07-09 13:06:04 +0100

    added render.c

---
+++ render/render.c
@@ -0,0 +1,41 @@
+# include "../cub3d_parsing.h"
+
+void init_renderer(t_cub3d *app, int screen_w, int screen_h)
+{
+    app->mlx.mlx_ptr   = mlx_init();
+    app->mlx.win_ptr   = mlx_new_window(app->mlx.mlx_ptr,
+                                        screen_w, screen_h,
+                                        "cub3d");
+    app->mlx.img_ptr   = mlx_new_image(app->mlx.mlx_ptr,
+                                       screen_w, screen_h);
+    app->mlx.img_data  = mlx_get_data_addr(app->mlx.img_ptr,
+        &app->mlx.bpp, &app->mlx.line_length, &app->mlx.endian);
+    app->mlx.width     = screen_w;
+    app->mlx.height    = screen_h;
+}
+
+void destroy_renderer(t_cub3d *app)
+{
+    if (app->mlx.img_ptr)
+        mlx_destroy_image(app->mlx.mlx_ptr, app->mlx.img_ptr);
+    if (app->mlx.win_ptr)
+        mlx_destroy_window(app->mlx.mlx_ptr, app->mlx.win_ptr);
+}
+
+void render_frame(t_cub3d *app)
+{
+    int x;
+
+    x = 0;
+    while (x < app->mlx.width)
+    {
+        // TODO: cast a ray for column x,
+        //       compute wall slice height & texture offset,
+        //       sample texture, and write pixels into app->mlx.img_data.
+        x++;
+    }
+    mlx_put_image_to_window(app->mlx.mlx_ptr,
+                            app->mlx.win_ptr,
+                            app->mlx.img_ptr,
+                            0, 0);
+}
