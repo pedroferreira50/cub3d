78ec4fa0978b8d78a90c0d07b89a50d0ba71ac66
Grace <20680751+godonnell2@users.noreply.github.com>
2025-07-09 13:17:15 +0100

    added screen width and height and init renderer

--- cub3d_parsing.h
+++ cub3d_parsing.h
@@ -53,6 +53,8 @@ typedef struct s_cub_elements
 	char	*so_texture;
 	char	*we_texture;
 	char	*ea_texture;
+  int               screen_width; //just realised i need this sorry 
+  int               screen_height;
 	int		floor_color[3];// could we make a sep colour struct for these? and put a ptr to it in here?? 
 	int		ceiling_color[3]; // could we make a sep colour struct for these? and put a ptr to it in here?? 
 	char	**map;
--- render/render.c
+++ render/render.c
@@ -1,20 +1,21 @@
-# include "../cub3d_parsing.h"
+#include "../cub3d_parsing.h"
 
-void init_renderer(t_cub3d *app, int screen_w, int screen_h)
+// app var means it holds everything i need at run time
+void init_renderer(t_cub_elements *app, int screen_w, int screen_h)
 {
-    app->mlx.mlx_ptr   = mlx_init();
-    app->mlx.win_ptr   = mlx_new_window(app->mlx.mlx_ptr,
-                                        screen_w, screen_h,
-                                        "cub3d");
-    app->mlx.img_ptr   = mlx_new_image(app->mlx.mlx_ptr,
-                                       screen_w, screen_h);
-    app->mlx.img_data  = mlx_get_data_addr(app->mlx.img_ptr,
-        &app->mlx.bpp, &app->mlx.line_length, &app->mlx.endian);
-    app->mlx.width     = screen_w;
-    app->mlx.height    = screen_h;
+    app->mlx.mlx_ptr = mlx_init();
+    app->mlx.win_ptr = mlx_new_window(app->mlx.mlx_ptr,
+                                      screen_w, screen_h,
+                                      "cub3d");
+    app->mlx.img_ptr = mlx_new_image(app->mlx.mlx_ptr,
+                                     screen_w, screen_h);
+    app->mlx.img_data = mlx_get_data_addr(app->mlx.img_ptr,
+                                          &app->mlx.bpp, &app->mlx.line_length, &app->mlx.endian);
+    app->mlx.width = screen_w;
+    app->mlx.height = screen_h;
 }
 
-void destroy_renderer(t_cub3d *app)
+void destroy_renderer(t_cub_elements *app)
 {
     if (app->mlx.img_ptr)
         mlx_destroy_image(app->mlx.mlx_ptr, app->mlx.img_ptr);
@@ -22,7 +23,7 @@ void destroy_renderer(t_cub3d *app)
         mlx_destroy_window(app->mlx.mlx_ptr, app->mlx.win_ptr);
 }
 
-void render_frame(t_cub3d *app)
+void render_frame(t_cub_elements *app)
 {
     int x;
 
